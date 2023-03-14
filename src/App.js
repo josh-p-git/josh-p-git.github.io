7c63286bdd787c757ac889635762cd1ed18e73ec
josh-p-git <joshua.penney91@gmail.com>
2023-03-14 11:25:15 +0000

    testing react deployment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Josh from "./components/Josh/Josh";
+
+function App() {
+  return (
+    <div>
+      <Josh />
+      {/* <Luiz />
+      <Juliana />
+      <Rachel /> */}
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Josh/Josh.js
@@ -0,0 +1,7 @@
+export default function Josh() {
+    return (
+        <div>
+            <h1>Josh test</h1>
+        </div>
+        )
+}
