ef3622cf08f8bb2d07c678180999ea69a42176ee
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-03-16 19:43:32 +0000

    Remove image background and update text position to justify

--- src/components/Josh/Josh.js
+++ src/components/Josh/Josh.js
@@ -1,17 +1,20 @@
 import './Josh.css';
 import ProfilePic from '../ProfilePic/ProfilePic'
-import josh from '../../images/Josh-no-colour.svg'
+import josh from '../../images/Josh.svg'
 import Info from '../personalInfo/Info';
 
 export default function Josh() {
-    return (
-      <div className='about'>
-        <div className='profile-pic'>
-          <ProfilePic imageProp={josh} />
-        </div>
-        <div>
-        <div className='name'>Josh Penney</div>
-            <p>
+  return (
+
+    <div className='pageContainer'>
+      <h1 className='name'>Josh Penney</h1>
+        <div className='aboutContainer'>
+          <div className='profile-pic'>
+            <ProfilePic imageProp={josh} />
+          </div>
+
+          <div>
+            <p className='aboutText'>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s, when an unknown printer took a galley of type
@@ -19,15 +22,16 @@ export default function Josh() {
             only five centuries, but also the leap into electronic typesetting,
             remaining essentially
             </p>
-            <Info 
+            
+            <Info
               git={"https://github.com/josh-p-git"}
               linkedin={"https://www.linkedin.com/in/jpenneynl/"}
               location={"Southampton"}
               phone={"072194321"}
               email={"josh.penney@gmail.com"}
               />
-        </div>
+            </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
