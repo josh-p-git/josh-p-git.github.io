e1e06e20eb832311830f8d9aa5fbd73564c0e028
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-03-28 16:45:28 +0100

    Added Ju CV

--- src/components/Juliana/Juliana.js
+++ src/components/Juliana/Juliana.js
@@ -1,15 +1,34 @@
-import '../profile-styling/profileStyling.css';
+import React from 'react';
+import { useState } from 'react';
+import './Juliana.css'
 import ProfilePic from '../ProfilePic/ProfilePic'
+import JulianaCV from '../../images/CVJuliana.pdf';
 import Ju from '../../images/Ju.svg'
 
 import Layout from '../layout/layout';
+import Portfolio from './Portfolio';
 import Email from '../personalInfo/Email';
 import House from '../personalInfo/House';
 import Linkedin from '../personalInfo/Linkedin';
 import Phone from '../personalInfo/Phone';
 import Git from '../personalInfo/Git';
+import CV from '../CV/CV';
 
-export default function Juliana({ handlePortfolio, handleCV, toggleComponents }) {
+export default function Juliana() {
+  
+  const [showPortfolio, setShowPortfolio] = useState(false)
+  const [showCV, setShowCV] = useState(false)
+
+  const handlePortfolio = () => {
+    setShowPortfolio(!showPortfolio)
+    setShowCV(false)
+  }
+
+  const handleCV = () => {
+    setShowCV(!showCV)
+    setShowPortfolio(false)
+  }
+  
   return (
     <Layout>
       <div className='pageContainer'>
@@ -21,12 +40,14 @@ export default function Juliana({ handlePortfolio, handleCV, toggleComponents })
 
             <div>
               <p className='aboutText'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of type
-              and scrambled it to make a type specimen book. It has survived not
-              only five centuries, but also the leap into electronic typesetting,
-              remaining essentially
+              I am a career changer junior software engineer with experience in 
+              front end languages and frameworks such as HTML, CSS, JavaScript, React, 
+              and React Native to create stunning user interfaces; back end technologies 
+              and frameworks, including NodeJS and ExpressJS; database management 
+              systems, such as PostgreSQL and MongoDB; and version control to 
+              collaborate effectively with other developers on large-scale projects.
+              I value continuous learning (curently working my way through C# and Phyton) 
+              and I am passionate about building strong relationships.
               </p>
               </div>
 
@@ -51,17 +72,17 @@ export default function Juliana({ handlePortfolio, handleCV, toggleComponents })
                 />
               </div>
 
-            <div className='buttons-container'>
+              <div className='buttons-container'>
               <button className='buttons' onClick={() => {
-                  handlePortfolio(true)
-                  toggleComponents(false)
+                  handlePortfolio()
               }}>My Portfolio</button>
               
               <button className='buttons' onClick={() => {
-                  handleCV(true)
-                  toggleComponents(false)
+                handleCV()
               }}>My CV</button>
             </div>
+            {showPortfolio ? <Portfolio />  : null}
+            {showCV ? <CV pdf={JulianaCV}/> : null}
       </div>
     </Layout>
   );
